d691aa883c230f7026df0614e19e2303786b6fb4
Lucas <lucasti@live.com>
2022-10-13 20:18:23 -0300

    storybook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
 
   return (
     <>
-      <h1 className="font-bold text-2xl text-violet-300">Hello World</h1>
+      <h1 className="font-bold text-6xl text-violet-300">Hello World</h1>
       <button className='bg-violet-500 font-medium px-4 rounded text-white hover:bg-violet-800'>ENVIAR</button>
     </>
   )
